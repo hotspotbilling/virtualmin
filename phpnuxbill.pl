ec378b0b5b7d205941e1970ec7a997985b747fd8
iBNu Maksum <me@ibnux.net>
2024-02-18 16:26:56 +0700

    Create phpnuxbill.pl

---
+++ phpnuxbill.pl
@@ -0,0 +1,29 @@
+
+sub script_phpnuxbill_desc
+{
+return "PHPNuxBill";
+}
+
+sub script_phpnuxbill_uses
+{
+return ( "php" );
+}
+
+sub script_phpnuxbill_category
+{
+return "Commerce";
+}
+
+sub script_phpnuxbill_php_vers
+{
+return ( 7, 8 );
+}
+
+sub script_phpnuxbill_php_modules
+{
+return ("mysql", "curl", "gd");
+}
+
+
+
+1;
