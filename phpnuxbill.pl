868f984ba2e029dd9a3b59d7025c28b013698efd
iBNu Maksum <me@ibnux.net>
2024-02-18 16:43:14 +0700

    add some check
    not yet finished

--- phpnuxbill.pl
+++ phpnuxbill.pl
@@ -4,6 +4,12 @@ sub script_phpnuxbill_desc
 return "PHPNuxBill";
 }
 
+sub script_phpnuxbill_longdesc
+{
+return "A PHP Mikrotik Billing";
+}
+
+
 sub script_phpnuxbill_uses
 {
 return ( "php" );
@@ -21,9 +27,17 @@ return ( 7, 8 );
 
 sub script_phpnuxbill_php_modules
 {
-return ("mysql", "curl", "gd");
+return ("mysql", "curl", "gd", "zip", "openssl","xml", "json");
 }
 
+sub script_phpnuxbill_php_vars
+{
+return ([ 'memory_limit', '128M', '+' ],
+        [ 'max_execution_time', 300, '+' ],
+        [ 'file_uploads', 'On' ],
+        [ 'upload_max_filesize', '20M', '+' ],
+        [ 'post_max_size', '20M', '+' ]);
+}
 
 
 1;
